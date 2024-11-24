ed77cd8e6e29f5ec0392b1da5fc8c6171cd9c731
seangah8 <seangah8@gmail.com>
2024-11-24 14:16:53 +0200

    testing github updates

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,7 +4,8 @@ export function AppHeader() {
     return (
         <header className="app-header full main-layout">
             <section className="header-container">
-                <h1>React Starter Proj</h1>
+                <h1>Test</h1>
+                <h1>Test2</h1>
             </section>
         </header>
     )
